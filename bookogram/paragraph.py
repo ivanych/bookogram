7e5c755c4b204c0fd1b0329d8a5845a7fd84405e
Mikhail Ivanov <m.ivanych@gmail.com>
2022-05-10 03:07:59 +0300

    Added the ability to randomly select variants

--- bookogram/paragraph.py
+++ bookogram/paragraph.py
@@ -1,3 +1,4 @@
+import random
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
@@ -10,12 +11,25 @@ def variants(p: dict):
     """
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text=variant_id,
-                    callback_data=f"{p['book_id']}_{variant_id}"
-                )
-                for variant_id in list(p.get('answers'))
-            ]
+            _random_variant(p) if p.get('random') == 'true' else _all_variants(p)
         ],
     )
+
+
+def _all_variants(p: dict):
+    return [
+        InlineKeyboardButton(
+            text=variant_id,
+            callback_data=f"{p['book_id']}_{variant_id}"
+        )
+        for variant_id in list(p.get('answers'))
+    ]
+
+
+def _random_variant(p: dict):
+    return [
+        InlineKeyboardButton(
+            text='  🎲  '.join(list(p.get('answers'))),
+            callback_data=f"{p['book_id']}_{random.choice(list(p.get('answers')))}"
+        )
+    ]
