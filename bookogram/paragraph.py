f44c0b52d1c24f20f78bbd77c4fe6e2be092e361
Mikhail Ivanov <m.ivanych@gmail.com>
2022-05-28 02:31:04 +0300

    Added class Paragraph

--- bookogram/paragraph.py
+++ bookogram/paragraph.py
@@ -1,5 +1,42 @@
 import random
+import hashlib
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import bookogram.answer as Answer
+
+
+# Параграф
+def paragraph(paragraph_dict: dict, book_id: str) -> dict:
+    pid = f"{book_id} {paragraph_dict.get('id')}"
+    sha = hashlib.sha3_256(pid.encode()).hexdigest()
+
+    answers = _answers(paragraph_dict.get('answers'), book_id)
+    print(f"└ ├ answers = {answers}")
+
+    paragraph = {
+        'book_id': book_id,
+        'id': paragraph_dict.get('id'),
+        'pid': pid,
+        'sha': sha,
+        'text': paragraph_dict.get('text'),
+        'answers': answers,
+        'random': paragraph_dict.get('random'),
+    }
+
+    return paragraph
+
+
+# Ответы
+def _answers(answers_list: list, book_id: str) -> list:
+    answers = []
+
+    for answer_dict in answers_list:
+        answer = Answer.answer(answer_dict, book_id)
+        print(f"├ answer = {answer}")
+
+        answers.append(answer)
+
+    return answers
 
 
 def answers_ikm(p: dict):
